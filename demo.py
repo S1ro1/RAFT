b3e4eae70cb0be2a76e1b151d9a86ff3de49e4ac
S1ro1 <matej.sirovatka@gmail.com>
2024-04-18 15:52:17 +0000

    Feat: demo for dataset creation

--- demo.py
+++ demo.py
@@ -1,75 +1,88 @@
-import sys
-sys.path.append('core')
+from pathlib import Path
+import sys  # isort:skip
 
-import argparse
-import os
-import cv2
-import glob
-import numpy as np
-import torch
-from PIL import Image
+sys.path.append("core")
 
-from raft import RAFT
-from utils import flow_viz
 from utils.utils import InputPadder
+from utils import flow_viz
+from raft import RAFT
+from PIL import Image
+import torch
+import numpy as np
+import os
+import argparse
+from torchvision.utils import save_image
+from tqdm import tqdm
+
+DEVICE = "cuda"
 
 
-
-DEVICE = 'cuda'
-
-def load_image(imfile):
+def load_image(imfile: str):
     img = np.array(Image.open(imfile)).astype(np.uint8)
     img = torch.from_numpy(img).permute(2, 0, 1).float()
     return img[None].to(DEVICE)
 
 
-def viz(img, flo):
-    img = img[0].permute(1,2,0).cpu().numpy()
-    flo = flo[0].permute(1,2,0).cpu().numpy()
-    
-    # map flow to rgb image
-    flo = flow_viz.flow_to_image(flo)
-    img_flo = np.concatenate([img, flo], axis=0)
+def convert_to_img(flow: torch.Tensor) -> torch.Tensor:
+    flow = flow[0].permute(1, 2, 0).cpu().numpy()
 
-    # import matplotlib.pyplot as plt
-    # plt.imshow(img_flo / 255.0)
-    # plt.show()
+    flow = flow_viz.flow_to_image(flow)
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.waitKey()
+    flow_tensor = torch.from_numpy(flow).permute(2, 0, 1) / 255.0
+    return flow_tensor
 
 
-def demo(args):
-    model = torch.nn.DataParallel(RAFT(args))
+@torch.no_grad()
+def convert_dataset(args):
+    model = torch.nn.DataParallel(RAFT(args), device_ids=args.devices)
     model.load_state_dict(torch.load(args.model))
 
     model = model.module
     model.to(DEVICE)
     model.eval()
 
-    with torch.no_grad():
-        images = glob.glob(os.path.join(args.path, '*.png')) + \
-                 glob.glob(os.path.join(args.path, '*.jpg'))
-        
-        images = sorted(images)
-        for imfile1, imfile2 in zip(images[:-1], images[1:]):
-            image1 = load_image(imfile1)
-            image2 = load_image(imfile2)
+    dataset_path = Path(args.dataset_path)
 
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+
+    for seq_path in tqdm(list(dataset_path.iterdir())):
+        output_dir = Path(args.outdir) / seq_path.name
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        first_frame_iter = sorted(list(seq_path.iterdir()))[:]
+        second_frame_iter = sorted(list(seq_path.iterdir()))[1:]
+        third_frame_iter = sorted(list(seq_path.iterdir()))[2:]
+        for first_frame, second_frame, third_frame in zip(first_frame_iter, second_frame_iter, third_frame_iter):
+            image1 = load_image(first_frame)
+            image2 = load_image(second_frame)
+            image3 = load_image(third_frame)
             padder = InputPadder(image1.shape)
-            image1, image2 = padder.pad(image1, image2)
+            image1, image2, image3 = padder.pad(image1, image2, image3)
+            print(image1)
+            print(image2.shape)
 
-            flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+            _, flow_up = model(image2, image3, iters=20, test_mode=True)
+            _, flow_down = model(image2, image1, iters=20, test_mode=True)
+
+            up_output_path = Path(args.outdir) / seq_path.name / (second_frame.name.removesuffix(".png") + "_up")
+            down_output_path = Path(args.outdir) / seq_path.name / (second_frame.name.removesuffix(".png") + "_down")
+
+            torch.save(flow_up, up_output_path.with_suffix(".pt"))
+            torch.save(flow_down, down_output_path.with_suffix(".pt"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help="restore checkpoint")
-    parser.add_argument('--path', help="dataset for evaluation")
-    parser.add_argument('--small', action='store_true', help='use small model')
-    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
-    parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
+    parser.add_argument("--model", help="restore checkpoint")
+    parser.add_argument("--small", action="store_true", help="use small model")
+    parser.add_argument("--mixed_precision", action="store_true", help="use mixed precision")
+    parser.add_argument("--alternate_corr", action="store_true", help="use efficent correlation implementation")
+    parser.add_argument("--devices", type=int, nargs="+", default=[0], help="devices to use")
+
+    parser.add_argument("--outdir", type=str, default="demo_flows", help="output directory for saving flows")
+    parser.add_argument("--dataset-path", type=str, default="data/frames", help="path to the dataset")
     args = parser.parse_args()
 
-    demo(args)
+    convert_dataset(args)
